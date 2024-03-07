abd1a647a7ba60a71a30eeae16f22d8aec69df4e
mahmadcoder <ahmadsadiq2284@gmail.com>
2024-03-08 00:48:55 +0500

    updated code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,9 @@
-// "use client"
-import FileUpload from "@/components/shared/FileUpload";
-import Description from "@/components/shared/Description";
-import Portfolio from "@/components/shared/Portfolio";
-import Instruction from "@/components/shared/Instruction";
+
 import Generate from "@/components/shared/Generate";
 import Acccordian from "@/components/shared/Accordian";
 export default function Home() {
   return (
     <>
-    {/* <FileUpload/>
-    <Description/>
-    <Portfolio/>
-    <Instruction/> */}
-    
     <Acccordian/>
     <Generate/>
     </>
